0450672e16a84f9851207105ca1205030f5fa7ab
Tony Chau <tonychau@Zazils-MBP.fios-router.home>
2024-06-26 17:24:55 -0400

    Added number buttons in html and css

---
+++ CSSPrac/Calculator.js
@@ -0,0 +1,23 @@
+let number1 = 0;
+let number2 = 0;
+let operator = "+";
+
+let add = (num1, num2) => {
+    return num1 + num2;
+}
+
+let subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+let multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+let divide = (num1, num2) => {
+    return num1 / num2;
+}
+
+let operate = (op, num1, num2) => {
+    
+}
